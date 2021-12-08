023a337884818ec7b0623951d087b78d1c9440fa
Viktor Szőke <viktor.szoke@snapsoft.hu>
2021-12-08 09:51:02 +0100

    Day 6 is almost done

--- utils/day-6.js
+++ utils/day-6.js
@@ -24,3 +24,58 @@ export const simulateLanternfishes = () => {
 
   console.log('Lanternfishes: ', tempLanternfieshes.length);
 };
+
+export const simulateBigLanternfishes = () => {
+  let tempLanternfieshes = lanternfishes;
+  let lanternfishesObject = {
+    zero: 0,
+    one: 0,
+    two: 0,
+    three: 0,
+    four: 0,
+    five: 0,
+    six: 0,
+    seven: 0,
+    eight: 0,
+  };
+
+  // Fill the obect
+  lanternfishesObject.zero = tempLanternfieshes.filter((x) => x === 0).length;
+  lanternfishesObject.one = tempLanternfieshes.filter((x) => x === 1).length;
+  lanternfishesObject.two = tempLanternfieshes.filter((x) => x === 2).length;
+  lanternfishesObject.three = tempLanternfieshes.filter((x) => x === 3).length;
+  lanternfishesObject.four = tempLanternfieshes.filter((x) => x === 4).length;
+  lanternfishesObject.five = tempLanternfieshes.filter((x) => x === 5).length;
+  lanternfishesObject.six = tempLanternfieshes.filter((x) => x === 6).length;
+  lanternfishesObject.seven = tempLanternfieshes.filter((x) => x === 7).length;
+  lanternfishesObject.eight = tempLanternfieshes.filter((x) => x === 8).length;
+
+  for (let i = 0; i < 256; i++) {
+    let newLanternfishCounter = 0;
+
+    for (const property in lanternfishesObject) {
+      // console.log(`${property}: ${lanternfishesObject[property]}`);
+      // lanternfishesObject[property]--;
+      if (lanternfishesObject['zero'] > 0) {
+        newLanternfishCounter++;
+      } else {
+        lanternfishesObject[property]--;
+      }
+    }
+
+    if (newLanternfishCounter > 0) {
+      lanternfishesObject['zero'] = 0;
+      lanternfishesObject['six'] += newLanternfishCounter;
+      lanternfishesObject['eight'] += newLanternfishCounter;
+    }
+  }
+
+  let allLanternfishes = 0;
+
+  for (const property in lanternfishesObject) {
+    // console.log(`${property}: ${lanternfishesObject[property]}`);
+    allLanternfishes += lanternfishesObject[property];
+  }
+
+  console.log(allLanternfishes);
+};
